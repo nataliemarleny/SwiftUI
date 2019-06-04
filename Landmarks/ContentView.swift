9419d7087cdd9eea2baaf9002b4c0e05f362d412
Natalie Marleny <nataliemarleny@gmail.com>
2019-06-04 04:41:23 +0100

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Natalie Marleny on 04/06/2019.
+//  Copyright © 2019 Natalie Marleny. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
